9816b6310aecb5866b39d86a633fcaded77daa44
VladiislavVolkov <kali.laskaa@gmail.com>
2023-02-09 22:54:53 +0300

    lesson-07-02

--- src/index.js
+++ src/index.js
@@ -1,8 +1,94 @@
-import { Header } from './components/Header/Header';
-import './main.scss';
+// import { Header } from './components/Header/Header';
+// import './main.scss';
 
-const root = document.querySelector('#root');
+// const root = document.querySelector('#root');
 
-root.insertAdjacentHTML('beforeend', Header());
+// root.insertAdjacentHTML('beforeend', Header());
 
-console.log('Hello IT-academy student 🚀');
+// console.log('Hello IT-academy student 🚀');
+
+// class Button {
+//   constructor(text, className) {
+//     this.state = {
+//       isActive: false,
+//       isFocused: false,
+//       text,
+//       className,
+//     };
+//   }
+
+//   onClick() {
+//     this.state.isActive = true;
+//   }
+
+//   render() {
+//     return `
+//         <button
+//             class='
+//                 ${this.state.className}
+//                 ${this.state.isActive ? 'active' : ''}
+//             '>
+//             ${this.state.text}
+//         </button>`;
+//   }
+// }
+
+// const btn1 = new Button('click');
+
+class TodoList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  add(title) {
+    this.tasks.push({
+      title,
+      id: this.tasks.length + 1,
+      isCompleted: false,
+    });
+  }
+
+  delete(id) {
+    this.tasks = this.tasks.filter((item) => item.id !== id);
+  }
+
+  complete(id) {
+    this.tasks = this.tasks.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isCompleted: true,
+        };
+      }
+      return item;
+    });
+  }
+
+  edit(task) {
+    const { id, title, isCompleted } = task;
+    this.tasks = this.tasks.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title,
+          isCompleted,
+        };
+      }
+      return item;
+    });
+  }
+
+  showAllTasks() {
+    if (this.tasks.length) {
+      this.tasks.forEach((item) => {
+        console.log(item);
+      });
+    } else {
+      console.log('нет задач');
+    }
+  }
+}
+
+const app = new TodoList();
+
+app.showAllTasks();
