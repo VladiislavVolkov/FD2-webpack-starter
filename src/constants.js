f4b09b9c57bc077c4e4c75bd8b85500b062d6004
VladiislavVolkov <kali.laskaa@gmail.com>
2023-02-14 20:03:52 +0300

    drumpad-exit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const KEYS = {
+  KeyA: 'KeyA',
+  KeyS: 'KeyS',
+  KeyD: 'KeyD',
+  KeyF: 'KeyF',
+  KeyG: 'KeyG',
+  KeyH: 'KeyH',
+};
+
+export const KEY_MAP = {
+  [KEYS.KeyA]: '../assets/sounds/kick.wav',
+  [KEYS.KeyS]: '../assets/sounds/cymbal.wav',
+  [KEYS.KeyD]: '../assets/sounds/snare.wav',
+  [KEYS.KeyF]: '../assets/sounds/openhat.wav',
+  [KEYS.KeyG]: '../assets/sounds/long-crash.wav',
+  [KEYS.KeyH]: '../assets/sounds/hihat.wav',
+};
--- src/index.js
+++ src/index.js
@@ -1,62 +1,41 @@
 import './main.scss';
+import { KEY_MAP, KEYS } from './constants';
 
 const pads = document.querySelectorAll('.pad');
 const visual = document.querySelector('.visual');
 
-export const KEY_MAP = {
-  kick: 'a',
-  cymbal: 's',
-  snare: 'd',
-  openHat: 'f',
-  longCrash: 'g',
-  hitKat: 'h',
-};
-
-export const SOUNDS = [
-  '../assets/sounds/kick.wav',
-  '../assets/sounds/cymbal.wav',
-  '../assets/sounds/snare.wav',
-  '../assets/sounds/openhat.wav',
-  '../assets/sounds/long-crash.wav',
-  '../assets/sounds/hihat.wav',
-];
-
-const playSound = (index) => {
-  const sound = SOUNDS[index];
+const playSound = (key) => {
+  const sound = KEY_MAP[key];
   const audio = new Audio(sound);
   audio.load();
   audio.play();
 };
 
-const onKeyPress = ({ key }) => {
-  switch (key) {
-    case 'a':
-      playSound(0);
-      console.log('enter A');
-      break;
-    case 's':
-      playSound(1);
-      console.log('enter S');
-      break;
-    case 'd':
-      playSound(2);
-      console.log('enter D');
-      break;
-    case 'f':
-      playSound(3);
-      console.log('enter F');
-      break;
-    case 'g':
-      playSound(4);
-      console.log('enter G');
-      break;
-    case 'h':
-      playSound(5);
-      console.log('enter H');
-      break;
-    default:
-      console.error('Error!!!');
-      break;
+const animatePad = (key) => {
+  const pad = Array.from(pads).find((item) => item.dataset.key === key);
+  pad.style.animation = 'dance .3s linear';
+  pad.addEventListener('animationend', () => {
+    pad.style.animation = 'none';
+  });
+};
+
+const createBuble = () => { 
+  const element = document.createElement('div');
+  visual.append(element);
+  element.style.animation = 'jump 1s linear';
+  element.style.background = 'red';
+  element.addEventListener('animationend', () => {
+    element.remove();
+  })
+};
+
+const onKeyPress = ({ code }) => {
+  if (KEYS[code]) {
+    playSound(code);
+    animatePad(code);
+    createBuble();
+  } else {
+    console.log('Error!');
   }
 };
 
