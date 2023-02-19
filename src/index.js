a00f6971c42961d693998990d04cc18959a5faee
VladiislavVolkov <kali.laskaa@gmail.com>
2023-02-19 14:24:53 +0300

    webComponent start

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import './components/SearchDropdown/SearchDropdown';
-import './main.scss';
+class WebComponentCard extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <div>
+            <header>
+                <h1>Title</h1>
+            </header>
+            <div>
+                <p>Content</p>
+            </div>
+            <button>Click</button>
+        </div>
+    `;
+  }
+}
+
+customElements.define('it-WebComponentCard', WebComponentCard);
